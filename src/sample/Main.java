96fd5b171f2482ce4db7e1198f19e2cd6b424f6a
Jonathan Pirca Chang <730134@ACDM.DS.SAIT.CA>
2019-04-17 22:46:28 -0600

    menu update
    meu update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,11 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
