7a7f9cdc9f50e08897183aecfbfd8c4cc0390e26
Jonathan Pirca <jpirca@gmail.com>
2019-04-23 09:39:06 -0600

    Changes
    Products

--- src/sample/controller/MainProgram.java
+++ src/sample/controller/MainProgram.java
@@ -1,19 +1,16 @@
 package sample.controller;
 
-import com.jfoenix.controls.JFXTreeTableColumn;
-import com.jfoenix.controls.JFXTreeTableView;
-import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.*;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
-import javafx.beans.InvalidationListener;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
@@ -23,14 +20,30 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Callback;
 import sample.model.Package;
+import sample.model.ProductSupplier;
+
 import java.net.URL;
-import java.util.Date;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class MainProgram implements Initializable {
 
+    ObservableList<Package> packageList = FXCollections.observableArrayList();
+
+    @FXML
+    private TableView<Package> tbl_Packages;
+
     @FXML
     private JFXTreeTableView<Package> tvPackages;
+
+    @FXML
+    private ListView<ProductSupplier> lst_ProdSup;
 
     @FXML
     private ImageView img_btn_package;
@@ -53,6 +66,33 @@ public class MainProgram implements Initializable {
     @FXML
     private Pane pan_settings;
 
+    @FXML
+    private JFXTextField txt_PackageName;
+
+    @FXML
+    private JFXTextField txt_PackageID;
+
+    @FXML
+    private JFXTextField txt_PckStartDate;
+
+    @FXML
+    private JFXTextField txt_PckEndDate;
+
+    @FXML
+    private JFXTextField txt_PckDesc;
+
+    @FXML
+    private JFXTextField txt_PckBasePrice;
+
+    @FXML
+    private JFXTextField txt_AgencyCommission;
+
+    @FXML
+    private JFXButton btn_InsPackage;
+
+    @FXML
+    private JFXButton btn_NewPackage;
+
 
 
 
@@ -65,24 +105,27 @@ public class MainProgram implements Initializable {
         styleMenuButtons(img_btn_logout);
         // Call method to print the package table
         printPackageTable();
+        fillPackageDetails(0);
+        fillProductSupplier(0);
 
 
-        tvPackages.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TreeItem<Package>>() {
-            @Override
-            public void changed(ObservableValue<? extends TreeItem<Package>> observable, TreeItem<Package> oldValue, TreeItem<Package> newValue) {
-                if(tvPackages.getSelectionModel().getSelectedItem() != null){
-                    TreeTableView.TreeTableViewSelectionModel<Package> selecModel = tvPackages.getSelectionModel();
-                    ObservableList selectCell = selecModel.getSelectedCells();
-                    TablePosition tp = (TablePosition) selectCell.get(0);
-                    Package pck = (Package) tp.getTableColumn().getCellData(newValue);
-                    System.out.println("Selected value" + pck.pkgName);
 
-
-                }
-            }
-        });
 
     }
+
+    private void fillProductSupplier(int pos) {
+        int pkgId = tbl_Packages.getItems().get(pos).getPackageId();
+        List<ProductSupplier> listPS = PackagesDB.getProductSupplierByPkg(pkgId);
+
+        for (ProductSupplier ps : listPS)
+        {
+            lst_ProdSup.getItems().add(ps);
+        }
+
+
+    }
+
+
 
     private void styleMenuButtons(ImageView menuButton) {
         // set a clip to apply rounded border to the original image.
@@ -121,99 +164,64 @@ public class MainProgram implements Initializable {
 
 
     private void printPackageTable() {
-        ObservableList<Package> packageList = FXCollections.observableArrayList();
+
         packageList = PackagesDB.getAllPackage();
 
         // Column: ID
-        JFXTreeTableColumn<Package,String> tiPckId = new JFXTreeTableColumn("ID");
-        tiPckId.setPrefWidth(30.00);
-        tiPckId.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Package, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Package, String> param) {
-                return param.getValue().getValue().packageId; // :D
-            }
-        });
-
-
+        TableColumn<Package,Integer> colPackageId = new TableColumn<>("ID");
+        colPackageId.setPrefWidth(30.00);
+        colPackageId.setCellValueFactory(new PropertyValueFactory<Package, Integer>("packageId"));
 
         // Column NAME
-        JFXTreeTableColumn<Package,String> tiName = new JFXTreeTableColumn("Name");
-        tiName.setPrefWidth(140.00);
+        TableColumn<Package,String> colPckName = new TableColumn<>("Name");
+        colPckName.setPrefWidth(140.00);
+        colPckName.setCellValueFactory(new PropertyValueFactory<Package, String>("pkgName"));
 
-        tiName.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Package, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Package, String> param) {
-                return param.getValue().getValue().pkgName;
-            }
-        });
+        // Column START DATE
+        TableColumn<Package,String> colPckStartDate = new TableColumn<>("Start Date");
+        colPckStartDate.setPrefWidth(90.00);
+        colPckStartDate.setCellValueFactory(new PropertyValueFactory<Package, String>("pkgStartDate"));
 
-//        // Column START DATE
-        JFXTreeTableColumn<Package,String> tiStartD = new JFXTreeTableColumn("Start Date");
-        tiStartD.setPrefWidth(90.00);
+        // Column END DATE
+        TableColumn<Package,String> colPckEndDate = new TableColumn<>("End Date");
+        colPckEndDate.setPrefWidth(90.00);
+        colPckEndDate.setCellValueFactory(new PropertyValueFactory<Package, String>("pkgEndDate"));
 
-        tiStartD.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Package, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Package, String> param) {
-                return param.getValue().getValue().pkgStartDate;
-            }
-        });
-//
-//        // Column END DATE
-        JFXTreeTableColumn<Package,String> tiEndD = new JFXTreeTableColumn("End Date");
-        tiEndD.setPrefWidth(90.00);
+        // Column DESCRIPTION
+        TableColumn<Package,String> colPckDesc = new TableColumn<>("Description");
+        colPckDesc.setPrefWidth(280.00);
+        colPckDesc.setCellValueFactory(new PropertyValueFactory<Package, String>("pkgDesc"));
 
-        tiEndD.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Package, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Package, String> param) {
-                return param.getValue().getValue().pkgEndDate;
-            }
-        });
-//
-//        // Column DESCRIPTION
-        JFXTreeTableColumn<Package,String> tiDesc = new JFXTreeTableColumn("Description");
-        tiDesc.setPrefWidth(280.00);
+        // Column PRICE
+        TableColumn<Package,Double> colPckPrice = new TableColumn<>("Price");
+        colPckPrice.setPrefWidth(100.00);
+        colPckPrice.setCellValueFactory(new PropertyValueFactory<Package, Double>("pkgBasePrice"));
 
-        tiDesc.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Package, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Package, String> param) {
-                return param.getValue().getValue().pkgDesc;
-            }
-        });
-//
-//        // Column PRICE
-        JFXTreeTableColumn<Package,String> tiBaseP = new JFXTreeTableColumn("Base Price");
-        tiBaseP.setPrefWidth(100.00);
+        // Column COMMISSION
+        TableColumn<Package,Double> colPckAgencyCommission = new TableColumn<>("Commission");
+        colPckAgencyCommission.setPrefWidth(100.00);
+        colPckAgencyCommission.setCellValueFactory(new PropertyValueFactory<Package, Double>("pkgAgencyCommission"));
 
-        tiBaseP.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Package, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Package, String> param) {
-                return param.getValue().getValue().pkgBasePrice.asString();
-            }
-        });
-//
-//        // Column COMMISSION
-        JFXTreeTableColumn<Package,String> tiCommi = new JFXTreeTableColumn("Commission");
-        tiCommi.setPrefWidth(100.00);
+        // Adding list with packages to the table
+        tbl_Packages.setItems(packageList);
+        // Adding column title to the table
+        tbl_Packages.getColumns().addAll(colPackageId,colPckName,colPckStartDate,colPckEndDate,colPckDesc,colPckPrice,colPckAgencyCommission);
+    }
 
-        tiCommi.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Package, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Package, String> param) {
-                return param.getValue().getValue().pkgAgencyCommission.asString();
-            }
-        });
+    @FXML
+    void on_ClickTablePackage(MouseEvent event) {
+        int position = tbl_Packages.getSelectionModel().selectedIndexProperty().get();
+        fillPackageDetails(position);
+    }
 
-//        ObservableList<Package> packageListNew = FXCollections.observableArrayList();
-//        packageListNew.add(new Package(1,"Package 1", new Date(),new Date(), "Description 1", 2000.00, 100.00));
-//        packageListNew.add(new Package(2,"Package 2", new Date(),new Date(), "Description 1", 2000.00, 100.00));
-
-
-        final TreeItem<Package> root = new RecursiveTreeItem<Package>(packageList, RecursiveTreeObject::getChildren);
-        //tvPackages.getColumns().setAll(tiName,tiStartD,tiEndD,tiDesc,tiBaseP,tiCommi);
-        tvPackages.getColumns().setAll(tiPckId, tiName,tiDesc,tiStartD,tiEndD,tiBaseP,tiCommi);
-        tvPackages.setRoot(root);
-        tvPackages.setShowRoot(false);
-
-
+    private void fillPackageDetails(int position) {
+        txt_PackageID.setText(String.valueOf(packageList.get(position).getPackageId()));
+        txt_PackageName.setText(String.valueOf(packageList.get(position).getPkgName()));
+        txt_PckStartDate.setText(String.valueOf(packageList.get(position).getPkgStartDate()));
+        txt_PckEndDate.setText(String.valueOf(packageList.get(position).getPkgEndDate()));
+        txt_PckDesc.setText(packageList.get(position).getPkgDesc());
+        txt_PckBasePrice.setText(String.valueOf(packageList.get(position).getPkgBasePrice()));
+        txt_AgencyCommission.setText(String.valueOf(packageList.get(position).getPkgAgencyCommission()));
     }
 
     @FXML
@@ -275,5 +283,65 @@ public class MainProgram implements Initializable {
         pan_settings.setVisible(true);
         pan_products.setVisible(false);
         pan_packages.setVisible(false);
+    }
+
+    @FXML
+    void on_ClickBtnUpdate(ActionEvent event) {
+        boolean result = false;
+        Package oldPck;
+
+        if (tbl_Packages.getSelectionModel().getSelectedItem() == null)
+        {
+            oldPck = tbl_Packages.getItems().get(0);
+        }
+        else {
+            oldPck = tbl_Packages.getSelectionModel().getSelectedItem();
+        }
+        Package newPck = null;
+        newPck = new Package(Integer.parseInt(txt_PackageID.getText()),
+                                            txt_PackageName.getText(),
+                                            oldPck.getPkgStartDate(),
+                                            oldPck.getPkgEndDate(),
+                                            txt_PckDesc.getText(),
+                                            Double.parseDouble(txt_PckBasePrice.getText()),
+                                            Double.parseDouble(txt_AgencyCommission.getText()));
+        result = PackagesDB.updatePackage(newPck,oldPck);
+
+        if (result) {
+           Alert alert = new Alert(Alert.AlertType.INFORMATION,"The record was updated successfully");
+           alert.showAndWait();
+           packageList.removeAll();
+           tbl_Packages.getColumns().clear();
+           printPackageTable();
+        }
+    }
+
+    @FXML
+    void on_ClickBtnInsert(ActionEvent event) {
+        Package newPkg = new Package(0,
+                txt_PackageName.getText(),
+                Date.valueOf("2018-02-02"),
+                Date.valueOf("2019-02-02"),
+                txt_PckDesc.getText(),
+                Double.parseDouble(txt_PckBasePrice.getText()),
+                Double.parseDouble(txt_AgencyCommission.getText()));
+        boolean result = PackagesDB.insertPackage(newPkg);
+    }
+
+    @FXML
+    void on_ClickBtnNew(ActionEvent event) {
+        clearTextFields();
+        btn_InsPackage.setDisable(false);
+
+    }
+
+    private void clearTextFields() {
+        txt_PackageID.clear();
+        txt_PackageName.clear();
+        txt_PckStartDate.clear();
+        txt_PckEndDate.clear();
+        txt_PckDesc.clear();
+        txt_PckBasePrice.clear();
+        txt_AgencyCommission.clear();
     }
 }
